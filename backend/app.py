c6e81ed3ac158bb7b44627370c0140aa890b8878
Zaid1103 <zkureshi11@gmail.com>
2024-08-13 15:05:43 +0100

    8- adjusting

--- backend/app.py
+++ backend/app.py
@@ -29,7 +29,3 @@ def index(filename):
 import routes
 with app.app_context():
   db.create_all()
-
-if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000))  # Use the port from the environment or default to 5000
-  app.run(host='0.0.0.0', port=port, debug=False) 
