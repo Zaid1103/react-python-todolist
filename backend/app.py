16dc35ae48545bd505688a9688f9b259eeb9109f
Zaid1103 <zkureshi11@gmail.com>
2024-08-13 14:00:55 +0100

    6- adjusting ports

--- backend/app.py
+++ backend/app.py
@@ -31,4 +31,5 @@ with app.app_context():
   db.create_all()
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))  # Use the port from the environment or default to 5000
+  app.run(host='0.0.0.0', port=port, debug=False) 
