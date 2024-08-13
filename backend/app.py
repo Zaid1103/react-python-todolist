9faecd04ab7cc77ae7c6bd04262a7ec430a9f0bd
Zaid1103 <zkureshi11@gmail.com>
2024-08-13 13:08:06 +0100

    4- configured app.py for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 #update this file for deployment 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -11,6 +12,20 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+# render.com
+
+frontend_folder = os.path.abspath(os.path.join(os.getcwd(),"..","frontend","dist"))
+dist_folder =  frontend_folder
+
+#Server static files from "dist" folder under frontend directory 
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+  if not filename:
+    filename = "index.html"
+  return send_from_directory(dist_folder,filename)
+
+#api routes (create, delete and update tasks)
 import routes
 with app.app_context():
   db.create_all()
